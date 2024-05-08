8ed1af2f6bd10d6f3b18b20e72f188b0d09829db
maria <mariasharif.16@hotmail.com>
2024-05-08 15:33:13 +0100

    Area of a rectangle

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,17 @@
+
+
+public class Rectangle {
+
+//properties
+        int length;
+        int width;
+
+
+        // constructor
+public Rectangle(int inputLength, int inputWidth) {
+        this.length = inputLength;
+        this.width = inputWidth;
+}
+
+
+}
